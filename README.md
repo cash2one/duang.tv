3ac2eae5c48ff1f46709e7d11c5a4b3c653ee2f2
gaolinjie <gaolinjie@gmail.com>
2015-06-01 00:18:15 +0800

    index and post page;

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# duang.tv
+duang.tv
+=====
---
+++ dbstructure/data.sql
@@ -0,0 +1,109 @@
+INSERT INTO `feed_type` VALUES (1, '提出了问题');
+INSERT INTO `feed_type` VALUES (2, '回答了问题');
+INSERT INTO `feed_type` VALUES (3, '关注了问题');
+INSERT INTO `feed_type` VALUES (4, '下很多人关注了问题');
+INSERT INTO `feed_type` VALUES (5, '赞同了回答');
+INSERT INTO `feed_type` VALUES (6, '下很多人赞同了回答');
+INSERT INTO `feed_type` VALUES (7, '发布了文章');
+INSERT INTO `feed_type` VALUES (8, '评论了文章');
+INSERT INTO `feed_type` VALUES (9, '收藏了文章');
+INSERT INTO `feed_type` VALUES (10, '下很多人收藏了文章');
+INSERT INTO `feed_type` VALUES (11, '赞同了评论');
+INSERT INTO `feed_type` VALUES (12, '下很多人赞同了评论');
+INSERT INTO `feed_type` VALUES (13, '赞了问题');
+INSERT INTO `feed_type` VALUES (14, '下很多人赞了问题');
+INSERT INTO `feed_type` VALUES (15, '赞了文章');
+INSERT INTO `feed_type` VALUES (16, '下很多人赞了文章');
+
+
+
+INSERT INTO `notice_type` VALUES (1, '回答了你的问题');
+INSERT INTO `notice_type` VALUES (2, '关注了你的问题');
+INSERT INTO `notice_type` VALUES (3, '感谢了你的问题');
+INSERT INTO `notice_type` VALUES (4, '赞同了你的回答');
+INSERT INTO `notice_type` VALUES (5, '感谢了你的回答');
+INSERT INTO `notice_type` VALUES (6, '在问题中提到了你');
+INSERT INTO `notice_type` VALUES (7, '在回答中提到了你');
+INSERT INTO `notice_type` VALUES (8, '评论了你的文章');
+INSERT INTO `notice_type` VALUES (9, '收藏了你的文章');
+INSERT INTO `notice_type` VALUES (10, '感谢了你的文章');
+INSERT INTO `notice_type` VALUES (11, '赞同了你的评论');
+INSERT INTO `notice_type` VALUES (12, '感谢了你的评论');
+INSERT INTO `notice_type` VALUES (13, '在文章中提到了你');
+INSERT INTO `notice_type` VALUES (14, '在评论中提到了你');
+INSERT INTO `notice_type` VALUES (15, '关注了你');
+INSERT INTO `notice_type` VALUES (16, '赞了你的问题');
+INSERT INTO `notice_type` VALUES (17, '赞了你的文章');
+
+
+
+INSERT INTO `category` VALUES (1, '其它', 0);
+INSERT INTO `category` VALUES (2, '彩票、旅行、充值、票务', 0);
+INSERT INTO `category` VALUES (3, '营养保健', 0);
+INSERT INTO `category` VALUES (4, '食品饮料、酒类、生鲜', 0);
+INSERT INTO `category` VALUES (5, '母婴、玩具乐器', 0);
+INSERT INTO `category` VALUES (6, '汽车用品', 0);
+INSERT INTO `category` VALUES (7, '运动户外', 0);
+INSERT INTO `category` VALUES (8, '鞋靴、箱包、钟表、奢侈品', 0);
+INSERT INTO `category` VALUES (9, '个护化妆', 0);
+INSERT INTO `category` VALUES (10, '服饰内衣、珠宝首饰', 0);
+INSERT INTO `category` VALUES (11, '家居、家具、家装、厨具', 0);
+INSERT INTO `category` VALUES (12, '电脑、办公', 0);
+INSERT INTO `category` VALUES (13, '手机、数码、京东通信', 0);
+INSERT INTO `category` VALUES (14, '家用电器', 0);
+INSERT INTO `category` VALUES (15, '图书、音像、数字商品', 0);
+
+INSERT INTO `tag_type` VALUES (1, '默认', '其它');
+INSERT INTO `tag_type` VALUES (2, '文章类型', '关注的文章');
+INSERT INTO `tag_type` VALUES (3, '问题类型', '感兴趣的问题');
+INSERT INTO `tag_type` VALUES (4, '类目', '关注的类目');
+INSERT INTO `tag_type` VALUES (5, '品牌', '喜欢的品牌');
+INSERT INTO `tag_type` VALUES (6, '商品', '想买的东西');
+
+INSERT INTO `avatar` VALUES (1, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1co4egzj30wt0wtq5b.jpg', '男');
+INSERT INTO `avatar` VALUES (2, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cozuyhj30wt0wtgny.jpg', '男');
+INSERT INTO `avatar` VALUES (3, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cpp9e8j30wt0wtdhz.jpg', '男');
+INSERT INTO `avatar` VALUES (4, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cqqws2j30wt0wt40r.jpg', '男');
+INSERT INTO `avatar` VALUES (5, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1crpp62j30wt0wtdi2.jpg', '男');
+INSERT INTO `avatar` VALUES (6, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cs8hxcj30wt0wtjto.jpg', '男');
+INSERT INTO `avatar` VALUES (7, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1ctddfnj30wt0wtwgl.jpg', '男');
+INSERT INTO `avatar` VALUES (8, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cu8cm4j30wt0wtabq.jpg', '男');
+INSERT INTO `avatar` VALUES (9, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cvbmakj30wt0wtjtx.jpg', '男');
+
+INSERT INTO `avatar` VALUES (10, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1co4egzj30wt0wtq5b.jpg', '女');
+INSERT INTO `avatar` VALUES (11, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cozuyhj30wt0wtgny.jpg', '女');
+INSERT INTO `avatar` VALUES (12, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cpp9e8j30wt0wtdhz.jpg', '女');
+INSERT INTO `avatar` VALUES (13, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cqqws2j30wt0wt40r.jpg', '女');
+INSERT INTO `avatar` VALUES (14, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1crpp62j30wt0wtdi2.jpg', '女');
+INSERT INTO `avatar` VALUES (15, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cs8hxcj30wt0wtjto.jpg', '女');
+INSERT INTO `avatar` VALUES (16, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1ctddfnj30wt0wtwgl.jpg', '女');
+INSERT INTO `avatar` VALUES (17, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cu8cm4j30wt0wtabq.jpg', '女');
+INSERT INTO `avatar` VALUES (18, 'http://mmm-static.qiniudn.com/005z2Z7Jtw1ejp1cvbmakj30wt0wtjtx.jpg', '女');
+
+
+
+INSERT INTO `permission` VALUES (1, 0, '普通用户');
+INSERT INTO `permission` VALUES (2, 1, '高级用户');
+INSERT INTO `permission` VALUES (3, 2, '超级用户');
+INSERT INTO `permission` VALUES (4, 11, '普通管理员');
+INSERT INTO `permission` VALUES (5, 22, '高级管理员');
+INSERT INTO `permission` VALUES (6, 13, '超级管理员');
+INSERT INTO `permission` VALUES (7, -1, '禁止提问');
+INSERT INTO `permission` VALUES (8, -2, '禁止回答');
+
+
+INSERT INTO `balance_type` VALUES (1, '初始资本', '获得初始资本', '2000 铜币');
+INSERT INTO `balance_type` VALUES (2, '创建主题', '创建了', '主题');
+INSERT INTO `balance_type` VALUES (3, '创建回复', '创建了', '回复');
+INSERT INTO `balance_type` VALUES (4, '主题回复收益 ', '收到 ', '的回复');
+INSERT INTO `balance_type` VALUES (5, '赞同别人', '发送对', '的赞同');
+INSERT INTO `balance_type` VALUES (6, '收到赞同', '收到', '的赞同');
+INSERT INTO `balance_type` VALUES (7, '撤销赞同', '撤销对', '的赞同');
+INSERT INTO `balance_type` VALUES (8, '赞同被撤销', '赞同被', '撤销');
+INSERT INTO `balance_type` VALUES (9, '发送谢意', '发送对', '的谢意');
+INSERT INTO `balance_type` VALUES (10, '收到谢意', '收到', '的谢意');
+INSERT INTO `balance_type` VALUES (11, '发送邀请', '发送', '邀请');
+INSERT INTO `balance_type` VALUES (12, '邀请成功', '邀请', '成功');
+
+INSERT INTO `ads` VALUES (1, 1, 1, 'http://mmm-cdn.qiniudn.com/1111.png', '/');
+
